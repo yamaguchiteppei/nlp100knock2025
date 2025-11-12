d53caca05a5c74d43d06f7008c1a8c9f82b9772d
yamaguchiteppei <k520484x@mails.cc.ehime-u.ac.jp>
2025-11-12 13:42:29 +0900

    第2章の最新バージョンをpushしました。

--- 2章/0211.py
+++ 2章/0211.py
@@ -3,3 +3,4 @@ with open('popular-names.txt','r') as f:
     for i in range(N):
         line = f.readline()
         print(line)
+
--- 2章/0217.py
+++ 2章/0217.py
@@ -1,5 +1,3 @@
-#まず一列目を全て取り出す。
-#一列目を取り出したら、それがリストなければ格納して、リストにあれば格納しないようにしてください。
 list = []
 with open('popular-names.txt','r') as f: 
     for line in f:
